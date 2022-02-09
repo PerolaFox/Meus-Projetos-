c4ba5757b6ac6a72234fbb4b65f9d62d0642eb60
PérolaFox <99212228+PerolaFox@users.noreply.github.com>
2022-02-09 15:46:24 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var caixa;
+function setup() {
+
+    createCanvas(600, 400)
+    
+    caixa =createSprite(400,400,200,200);
+    
+}
+
+function draw() {
+
+    background("blue");
+    
+    drawSprites();
+
+}
